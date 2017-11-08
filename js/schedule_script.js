f47928c38118f0848ed6496ecddd821987cc842d
gregg <gmicay123@gmail.com>
2017-11-08 22:11:18 +0800

    edit function done finally lorde

--- js/schedule_script.js
+++ js/schedule_script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 	$(document).on("click", "#add_constraint", addConstraint);
+	$(document).on("click", "#edit_constraint", editConstraint);
 	$(document).on("click", ".remove-constraint", removeConstraint);
+	$(document).on("click", ".edit-constraint", editModalOpen);
     $(document).on("click", ".constraint-item", changeBtnName);
+    $(document).on("hide.bs.modal", "#editconstraint", hideEditModal);
     $('[data-toggle="tooltip"]').tooltip();
     $(function(){
     	$('.timepicker3').datetimepicker({
@@ -9,6 +12,20 @@ $(document).ready(function(){
     	});
     });
 });
+
+
+function hideEditModal(){
+	$("#edit_tabs > li").removeClass("active");
+	$("#editcourserestriction").removeClass("active");
+	$("#editcourserestriction").removeClass("in");
+	$("#editmeetingtime").removeClass("active");
+	$("#editmeetingtime").removeClass("in");
+	$("#edit_tabs > li[data-tab=editcourserestriction]").addClass("active");
+	$("#editcourserestriction").addClass("active");
+	$("#editcourserestriction").addClass("in");
+	$("input:radio[name=edit_priority]").prop("checked", false);
+	$("input:checkbox[name=days]").prop("checked", false);
+}
 
 function changeBtnName(){
 	var text = $(this).text()
@@ -23,18 +40,149 @@ function changeBtnName(){
 	}
 }
 
+function editModalOpen(){
+	var constraint_type = $(this).parent().parent().data("constraint_type");
+	var priority = $(this).parent().parent().data("priority");
+	var musthave = $(this).parent().parent().data("musthave");
+	var course = $(this).parent().parent().data("course");
+	var start_time = $(this).parent().parent().data("start_time");
+	var end_time = $(this).parent().parent().data("end_time");
+	var days = $(this).parent().parent().data("days");
+	$("#edit_constraint").attr("data-constraint", $(this).parent().parent().data("id"));
+	$("#edit_constraint").attr("data-prev-priority", priority);
+	if(constraint_type == "meetingtime"){
+		// close course restriction tab
+		$("#edit_tabs > li[data-tab=editcourserestriction]").removeClass("active");
+		$("#editcourserestriction").removeClass("active");
+		$("#editcourserestriction").removeClass("in");
+		// open meeting time tab
+		$("#edit_tabs > li[data-tab=edit"+constraint_type+"]").addClass("active");
+		$("#edit"+constraint_type).addClass("active");
+		$("#edit"+constraint_type).addClass("in");
+		$("#edit-start-time").val(start_time);
+		$("#edit-end-time").val(end_time);
+		$("#editmeetingtime input:checkbox[name=days]").each(function(){
+			for (var i = days.length - 1; i >= 0; i--) {
+				if(days[i] == $(this).val()){
+					$(this).prop("checked", true);
+				}
+			}
+		});
+	}else{
+		var constraint_btn_html = "Must Not Have";
+		if(musthave == "musthave"){
+			constraint_btn_html = "Must Have";
+		}
+		$(".edit-constraint-btn").html(constraint_btn_html+" <span class='caret'></span>");
+		$(".edit-constraint-btn").attr("data-constraint", musthave);
+		$("input[name=edit_course]").val(course);
+	}
+	$("#radio_"+priority).prop("checked", true);
+}
+
+function editConstraint(e){
+	e.preventDefault();
+	var text = "";
+	var musthave = "";
+	var start_time = "";
+	var end_time = "";
+	if($("#edit_tabs > .active").attr("data-tab") == "editcourserestriction"){
+		constraint_type = "courserestriction";
+		text = "Must Not Have"
+		musthave = $(".edit-constraint-btn").attr("data-constraint");
+		if(musthave == "musthave"){
+			text = "Must Have";
+		}
+		text += (" "+$("#editcourserestriction > div > input:text[name=edit_course]").val());
+	}else{
+		constraint_type = "meetingtime";
+		start_time = $("#edit-start-time").val();
+		end_time = $("#edit-end-time").val();
+		text = "Classes must start from "+start_time+" to "+end_time;
+		if(start_time == end_time){
+			text = "No Classes";
+		}
+		var selected = [];
+		$("#editmeetingtime > div > label > input:checkbox[name=days]:checked").each(function(){
+			selected.push($(this).val());
+		});
+		if(selected.length){
+			text += " on ";
+			for (var i = 0; i < selected.length; i++) {
+				text += selected[i];
+				if(i != selected.length-1){
+					text += ", ";
+				}
+			}
+		}
+	}
+	constraintObject = {
+		id: $("#edit_constraint").attr("data-constraint"),
+		constraint_type: constraint_type,
+		priority: $("input:radio[name=edit_priority]:checked").val(),
+		musthave: musthave,
+		start_time: start_time,
+		end_time: end_time,
+		course: $("#addcourserestriction > div > input:text[name=edit_course]").val(),
+		days: selected
+	};
+	var priority_value = $("input:radio[name=edit_priority]:checked").val();
+	var prev_priority = $("#edit_constraint").attr("data-prev-priority");
+	var div_id = $("#edit_constraint").attr("data-constraint");
+	$("#"+div_id).html('<p>'+
+						'<b>'+text+'</b>'+
+						'<a class="remove-constraint" data-toggle="modal"  href="#remove" ><span class="glyphicon glyphicon-remove pull-right"></span></a>'+
+						'<a class="edit-constraint" data-toggle="modal" href="#editconstraint"><span class="glyphicon glyphicon-edit pull-right"></span></a>'+
+					'</p>');
+	if(priority_value != prev_priority){
+		new_div_id = priority_value+"_"+($("#"+priority_value+" > .panel-body > .priority_entry").length+1);
+		constraintObject.id = new_div_id;
+		newDiv = $("#"+div_id).remove();
+		newDiv.attr("id", new_div_id);
+		newDiv.data(constraintObject);
+		$("#"+prev_priority+"_badge").html($("#"+prev_priority+" > .panel-body > .priority_entry").length);
+		if ($("#"+priority_value+" > .panel-body > .no_entry").length){
+			$("#"+priority_value+" > .panel-body").html(newDiv);
+			$("#"+priority_value+"_badge").html(1);
+		}else{
+			$("#"+priority_value+" > .panel-body").append(newDiv);
+			$("#"+priority_value+"_badge").html($("#"+priority_value+" > .panel-body > .priority_entry").length);
+		}
+
+		if($("#"+prev_priority+" > .panel-body > .priority_entry").length == 0){
+			$("#"+prev_priority+" > .panel-body").html(
+					'<div class="priority_entry no_entry">'+
+						'<p>'+
+							'<b>No Constraints</b>'+
+						'</p>'+
+					'</div>'
+				);
+		}
+
+		$("#"+priority_value).addClass("in");
+	}
+	$("#editconstraint").modal('hide');
+}
+
 function addConstraint(e){
 	e.preventDefault();
 	var text = "";
+	var constraint_type = 0;
+	var musthave = "";
+	var start_time = "";
+	var end_time = "";
 	if($("#add_tabs > .active").attr("data-tab") == "addcourserestriction"){
+		constraint_type = "courserestriction";
 		text = "Must Not Have"
-		if($(".add-constraint-btn").attr("data-constraint") == "musthave"){
+		musthave = $(".add-constraint-btn").attr("data-constraint");
+		if(musthave == "musthave"){
 			text = "Must Have";
 		}
 		text += (" "+$("#addcourserestriction > div > input:text[name=course]").val());
 	}else{
-		var start_time = $("#add-start-time").val();
-		var end_time = $("#add-end-time").val();
+		constraint_type = "meetingtime"
+		start_time = $("#add-start-time").val();
+		end_time = $("#add-end-time").val();
 		text = "Classes must start from "+start_time+" to "+end_time;
 		if (start_time == end_time){
 			text = "No Classes";
@@ -53,14 +201,28 @@ function addConstraint(e){
 			}
 		}
 	}
-	var newDiv = '<div class="priority_entry">'+
+	var priority_value = "#"+$("input:radio[name=add_priority]:checked").val();
+	var constraint_num = ($(priority_value+" > .panel-body > .priority_entry").length)+1;
+	if($(priority_value+" > .panel-body > .no_entry").length){
+		constraint_num = 1;
+	}
+	constraintObject = {
+		id: $("input:radio[name=add_priority]:checked").val()+"_"+(constraint_num),
+		constraint_type: constraint_type,
+		priority: $("input:radio[name=add_priority]:checked").val(),
+		musthave: musthave,
+		start_time: start_time,
+		end_time: end_time,
+		course: $("#addcourserestriction > div > input:text[name=course]").val(),
+		days: selected
+	};
+	var newDiv = '<div class="priority_entry" id="'+$("input:radio[name=add_priority]:checked").val()+'_'+(constraint_num)+'">'+
 					'<p>'+
 						'<b>'+text+'</b>'+
 						'<a class="remove-constraint" data-toggle="modal"  href="#remove" ><span class="glyphicon glyphicon-remove pull-right"></span></a>'+
-						'<a class="" data-toggle="modal" href="#editconstraint"><span class="glyphicon glyphicon-edit pull-right"></span></a>'+
+						'<a class="edit-constraint" data-toggle="modal" href="#editconstraint"><span class="glyphicon glyphicon-edit pull-right"></span></a>'+
 					'</p>'+
 				'</div>';
-	var priority_value = "#"+$("input:radio[name=add_priority]:checked").val();
 	if ($(priority_value+" > .panel-body > .no_entry").length){
 		$(priority_value+" > .panel-body").html(newDiv);
 		$(priority_value+"_badge").html(1);
@@ -68,6 +230,7 @@ function addConstraint(e){
 		$(priority_value+" > .panel-body").append(newDiv);
 		$(priority_value+"_badge").html($(priority_value+" > .panel-body > .priority_entry").length);
 	}
+	$(priority_value+" > .panel-body > .priority_entry:last-child").data(constraintObject);
 	$(priority_value).addClass("in");
 	addConstraintReset();
 }
